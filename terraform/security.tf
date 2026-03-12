b60b7c7ee6b664e27bfd5032d9430107c5a5449a
wnstj999 <wnstj99999@gmail.com>
2026-03-12 15:08:47 +0900

    fix: allow proxy to reach frontend port 80 on app sg

--- terraform/security.tf
+++ terraform/security.tf
@@ -33,6 +33,14 @@ resource "aws_security_group" "app_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.proxy_sg.id]
+    description     = "Frontend from Proxy EC2"
+  }
+
+  ingress {
     from_port       = 8000
     to_port         = 8000
     protocol        = "tcp"
